29542561a18aab8ebf4a63d4687eda88aae77df3
Nolan G <nolan__g@hotmail.com>
2014-01-15 00:03:13 +0000

    started work on the mobile layouts

--- all/themes/lampton/templates/html.tpl.php
+++ all/themes/lampton/templates/html.tpl.php
@@ -9,8 +9,8 @@
 ?><!DOCTYPE html>
 <!--[if IEMobile 7]><html class="iem7" <?php print $html_attributes; ?>><![endif]-->
 <!--[if lte IE 6]><html class="lt-ie9 lt-ie8 lt-ie7" <?php print $html_attributes; ?>><![endif]-->
-<!--[if (IE 7)&(!IEMobile)]><link rel="stylesheet" href="/drupal/sites/all/themes/lampton/css/old-ie7.css"><html class="lt-ie9 lt-ie8" <?php print $html_attributes; ?>><![endif]-->
-<!--[if IE 8]><html class="lt-ie9" <?php print $html_attributes; ?>><link rel="stylesheet" href="/drupal/sites/all/themes/lampton/css/old-ie8.css"><![endif]-->
+<!--[if (IE 7)&(!IEMobile)]><link rel="stylesheet" href="/sites/all/themes/lampton/css/old-ie7.css"><html class="lt-ie9 lt-ie8" <?php print $html_attributes; ?>><![endif]-->
+<!--[if IE 8]><html class="lt-ie9" <?php print $html_attributes; ?>><link rel="stylesheet" href="/sites/all/themes/lampton/css/old-ie8.css"><![endif]-->
 <!--[if (gte IE 9)|(gt IEMobile 7)]><!-->
 <html <?php print $html_attributes . $rdf_namespaces; ?>><!--<![endif]-->
 
@@ -40,10 +40,12 @@
     <script src="<?php print $base_path . $path_to_zen; ?>/js/respond.js"></script>
     <![endif]-->
   <?php endif; ?>
-  <link href="/drupal/sites/all/libraries/bxslider/jquery.bxslider.css" rel="stylesheet" />
+  <link href="/sites/all/libraries/bxslider/jquery.bxslider.css" rel="stylesheet" />
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
   <?php if ($skip_link_text && $skip_link_anchor): ?>
+  <!--[if (IE 7)&(!IEMobile)]><link rel="stylesheet" href="/sites/all/themes/lampton/css/old-ie7.css"><html class="lt-ie9 lt-ie8" <?php print $html_attributes; ?>><![endif]-->
+  <!--[if IE 8]><html class="lt-ie9" <?php print $html_attributes; ?>><link rel="stylesheet" href="/sites/all/themes/lampton/css/old-ie8.css"><![endif]-->
     <p id="skip-link">
       <a href="#<?php print $skip_link_anchor; ?>" class="element-invisible element-focusable"><?php print $skip_link_text; ?></a>
     </p>
